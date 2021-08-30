56006fd206a9d04705780c56eefe6d1dfef2efad
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-08-30 15:35:16 -0300

    Tenta passar no requisito 11

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const creatTask = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
+const listClass = document.querySelector('.taskList');
 const allLi = document.getElementsByTagName('li');
 const eraseList = document.getElementById('apaga-tudo');
+const eraseSelected = document.getElementById('remover-finalizados');
 
 function addItem() {
   const input = document.getElementById('texto-tarefa').value;
@@ -16,13 +18,26 @@ function removeAllItens() {
   list.innerHTML = '';
 }
 
+// allLi.classList.add = 'li-list-item';
+// const liListItem = document.querySelectorAll('.li-lis-item');
+function removeSelected() {
+  for (let i = 0; i < allLi.length; i += 1) {
+    const element = allLi[i];
+
+    if (element.classList.contains('completed')) {
+      listClass.removeChild(element);
+      i -= 1;
+    }
+  }
+}
+
 function clickChange(event) {
   for (let i = 0; i < allLi.length; i += 1) {
     if (allLi[i].classList.contains('greySelected')) {
       allLi[i].classList.remove('greySelected');
     }
+    event.target.classList.add('greySelected');
   }
-  event.target.classList.add('greySelected');
 }
 
 function riskTask(event) {
@@ -35,3 +50,4 @@ creatTask.addEventListener('click', addItem);
 list.addEventListener('click', clickChange);
 list.addEventListener('dblclick', riskTask);
 eraseList.addEventListener('click', removeAllItens);
+eraseSelected.addEventListener('click', removeSelected);
